94cebb568090999fabf095ac85a4318a53c8ae00
IhsanRamadhan <rihsan900@gmail.com>
2026-05-08 14:22:18 +0700

    fix prisma migrate

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Syncing database schema..."
-./node_modules/.bin/prisma db push --skip-generate
+./node_modules/.bin/prisma db push
 
 echo "Starting server..."
 exec bun --bun run /app/server/index.mjs
